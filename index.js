7513af8a5908315e9fa1689f73497b3f5bdfa878
Gabriel Cavalcante <35786864+gcavalcantes@users.noreply.github.com>
2022-05-03 19:08:17 -0300

    Movement and Attack
    Finished polishing the basic movement and implemented a simple attack.
    Next up is health bar interface.

--- index.js
+++ index.js
@@ -6,23 +6,48 @@ canvas.height = 576;
 
 c.fillRect(0, 0, canvas.width, canvas.height)
 
-const gravity = 0.2
+const gravity = 0.7
 
 class Sprite{
-    constructor({position, velocity}){
+    constructor({position, velocity, color = 'red', offset}){
         this.position = position
         this.velocity = velocity
+        this.width = 50
         this.height = 150
         this.lastKey
+        this.attackBox = {
+            position: {
+                x: this.position.x,
+                y: this.position.y
+            },
+            offset,
+            width: 100,
+            height: 50,
+        }
+        this.color = color
+        this.isAttacking
     }
 
     draw(){
-        c.fillStyle = 'red'
-        c.fillRect(this.position.x, this.position.y, 50, this.height)
+        c.fillStyle = this.color
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+
+        if(this.isAttacking){
+            // This is where the attack box is drawn
+            c.fillStyle = 'green'
+            c.fillRect(
+                this.attackBox.position.x, 
+                this.attackBox.position.y, 
+                this.attackBox.width, 
+                this.attackBox.height,
+            )
+        }
     }
 
     update() {
         this.draw()
+        this.attackBox.position.x = this.position.x + this.attackBox.offset.x
+        this.attackBox.position.y = this.position.y
 
         this.position.x += this.velocity.x
         this.position.y += this.velocity.y
@@ -33,12 +58,23 @@ class Sprite{
             this.velocity.y += gravity
         }
     }
+
+    attack(){
+        this.isAttacking = true
+        setTimeout(() => {
+            this.isAttacking = false
+        }, 100)
+    }
 }
 
 const player = new Sprite({
     position: {
     x:0,
     y:0
+    },
+    offset: {
+        x: 0,
+        y: 0
     },
     velocity:{
         x:0,
@@ -51,10 +87,15 @@ const enemy = new Sprite({
     x:400,
     y:100
     },
+    offset: {
+        x: -50,
+        y: 0
+    },
     velocity:{
         x:0,
         y:0
-    }
+    },
+    color: 'blue'
 })
 
 console.log(player)
@@ -74,7 +115,14 @@ const keys = {
     }
 }
 
-let lastKey
+function rectangularCollision({rectangle1, rectangle2}){
+    return (
+        rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x && 
+        rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width && 
+        rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y &&
+        rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
+    )
+}
 
 function animate() {
     window.requestAnimationFrame(animate)
@@ -83,38 +131,63 @@ function animate() {
     player.update()
     enemy.update()
     
-    // Player 1 movement
+    // Player 1 movement 
     player.velocity.x = 0
-    if (keys.a.pressed && lastKey === 'a') {
-        player.velocity.x = -1
-    } else if (keys.d.pressed && lastKey === 'd'){
-        player.velocity.x = 1
+
+    if (keys.a.pressed && player.lastKey === 'a') {
+        player.velocity.x = -5
+    } else if (keys.d.pressed && player.lastKey === 'd'){
+        player.velocity.x = 5
     }
 
     // Player 2 movement
     enemy.velocity.x = 0
     if (keys.ArrowLeft.pressed && enemy.lastKey === 'ArrowLeft') {
-        enemy.velocity.x = -1
+        enemy.velocity.x = -5
     } else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight'){
-        enemy.velocity.x = 1
+        enemy.velocity.x = 5
+    }
+
+    // Detect for collision for player 1
+    if (
+        rectangularCollision({rectangle1: player, rectangle2: enemy}) &&
+        player.isAttacking
+        ){
+        player.isAttacking = false
+        console.log('Player 1 attack successful')
+
+    }
+
+    // Detect for collision for player 2
+    if (
+        rectangularCollision({rectangle1: enemy, rectangle2: player}) &&
+        enemy.isAttacking
+        ){
+        enemy.isAttacking = false
+        console.log('Player 2 attack successful')
+
     }
 }
 
 animate()
 
 window.addEventListener('keydown', (event) => {
-    console.log(event.key);
+
     switch (event.key){
+        // Player 1 keys down
         case 'd':
             keys.d.pressed = true
-            lastKey = 'd'
+            player.lastKey = 'd'
             break
         case 'a':
             keys.a.pressed = true
-            lastKey = 'a'
+            player.lastKey = 'a'
             break
         case 'w':
-            player.velocity.y = -10
+            player.velocity.y = -20
+            break
+        case ' ':
+            player.attack()
             break
         
         // Player 2 keys down
@@ -127,10 +200,13 @@ window.addEventListener('keydown', (event) => {
             enemy.lastKey = 'ArrowLeft'
             break
         case 'ArrowUp':
-            enemy.velocity.y = -10
+            enemy.velocity.y = -20
+            break
+        case 'ArrowDown':
+            enemy.attack()
             break
     }
-    console.log(event.key);
+
 })
 window.addEventListener('keyup', (event) => {
     // Player 1 keys up
@@ -158,5 +234,5 @@ window.addEventListener('keyup', (event) => {
             enemy.velocity.y = 0
             break
     }
-    console.log(event.key);
+
 })
