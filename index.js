8d71884b400a236d7531f2caf1bcf2bd84d4cad4
Gabriel Cavalcante <35786864+gcavalcantes@users.noreply.github.com>
2022-06-06 16:07:16 -0300

    There and back again
    There are no sprites for going backwords, so the same sprite is used for going forward.
    Next up is jumping sprites.

--- index.js
+++ index.js
@@ -45,6 +45,17 @@ const player = new Fighter({
     offset: {
         x: 215,
         y: 155,
+    },
+    sprites: {
+        idle: {
+            imageSrc: './img/samuraiMack/Idle.png',
+            framesMax: 8,
+        },
+        run: {
+            imageSrc: './img/samuraiMack/Run.png',
+            framesMax: 8,
+            
+        }
     }
 })
 
@@ -94,11 +105,13 @@ function animate() {
     
     // Player 1 movement 
     player.velocity.x = 0
-
+    player.image = player.sprites.idle.image
     if (keys.a.pressed && player.lastKey === 'a') {
         player.velocity.x = -5
+        player.image = player.sprites.run.image
     } else if (keys.d.pressed && player.lastKey === 'd'){
         player.velocity.x = 5
+        player.image = player.sprites.run.image
     }
 
     // Player 2 movement
